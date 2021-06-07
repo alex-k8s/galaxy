bd4dff90f064bf0d6a99f6b6ded837e0c92e0efc
almahmoud <amahmou4@jhu.edu>
2021-06-07 18:04:09 -0400

    ChatOps pre-GCC
    Original version
    
    Test with new scripts
    
    Make all commands dispatched
    
    Add gists
    
    grep to action replace
    
    Externalize log extraction
    
    Gist logs command
    
    Fix for params
    
    Tail logs
    
    worfklow typo
    
    Typo in tail comment

---
+++ .github/scripts/deploy_preview.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -o xtrace
+
+git diff --name-status "$PR_BASE" "$PR_HEAD"
+
+# Abort if anything but modified and added
+abort=$(git diff --name-status "$PR_BASE" "$PR_HEAD" | cut -c1 | grep -E "C|D|R|T|U|X|B" )
+
+
+if [[ ! -n $abort ]]; then
+
+    echo "No unsupported changes detected. Starting deployment."
+    
+    export GALAXYDIR="$(pwd)"
+
+    cd ..
+
+    export BASEDIR="$(pwd)"
+
+    git clone https://github.com/CloudVE/galaxy-k8s-dev-env
+    git clone https://github.com/galaxyproject/galaxy-helm
+    cd galaxy-helm/galaxy
+    helm dep update
+    cd ../..
+    export CHARTDIR="$(pwd)/galaxy-helm"
+    echo "Starting making symlinks"
+
+    cd galaxy-k8s-dev-env
+
+    bash symlink_branch.sh "pr-${PR_NUM}" "$GALAXYDIR" "$CHARTDIR"
+
+    bash update_links.sh "pr-${PR_NUM}" "$DEVENV_DOMAIN" "/galaxy-pr-${PR_NUM}" "$PR_BASE"
+
+    bash helm_update.sh "pr-${PR_NUM}" "pr-${PR_NUM}" $@ --set postgresql.deploy=false --wait > "$GALAXYDIR/gxyinstalloutput"
+
+fi
