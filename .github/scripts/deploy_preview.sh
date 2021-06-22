d37bda41ebbde79c1c0a77d8490505bb940de38d
Alexandru Mahmoud <almahmoud@jhu.edu>
2021-06-21 22:16:48 -0400

    Add hashed link

--- .github/scripts/deploy_preview.sh
+++ .github/scripts/deploy_preview.sh
@@ -24,13 +24,14 @@ if [[ ! -n $abort ]]; then
     helm dep update
     cd ../..
     export CHARTDIR="$(pwd)/galaxy-helm"
+    export HASHED_PR_NUM="$(sha1sum < <(printf '%s' \"galaxy-pr-${PR_NUM}\") | cut -f 1 -d ' ')"
     echo "Starting making symlinks"
 
     cd galaxy-k8s-dev-env
 
     bash symlink_branch.sh "pr-${PR_NUM}" "$GALAXYDIR" "$CHARTDIR"
 
-    bash update_links.sh "pr-${PR_NUM}" "$DEVENV_DOMAIN" "/galaxy-pr-${PR_NUM}" "$PR_BASE"
+    bash update_links.sh "pr-${PR_NUM}" "$DEVENV_DOMAIN" "/pr-${PR_NUM}-$HASHED_PR_NUM" "$PR_BASE"
 
     bash helm_update.sh "pr-${PR_NUM}" "pr-${PR_NUM}" $@ --set postgresql.deploy=false --wait > "$GALAXYDIR/gxyinstalloutput"
 
